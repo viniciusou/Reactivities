5f82d12cf8bd3e242c34575e9402d44d83031079
viniciusou <viniciusou@gmail.com>
2019-12-17 17:42:43 -0300

    Integrated api and react app

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import { Item, Button, Label, Segment } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 
 interface IProps {
     activities: IActivity[];
     selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
+    deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+    submitting: boolean;
+    target: string;
 }
 
 export const ActivityList: React.FC<IProps> = ({
     activities, 
     selectActivity,
-    deleteActivity
+    deleteActivity,
+    submitting,
+    target
 }) => {
     return (
         <Segment clearing>
@@ -26,8 +30,17 @@ export const ActivityList: React.FC<IProps> = ({
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
-                                <Button onClick={() => deleteActivity(activity.id)} floated='right' content='Delete' color='red' />
+                                <Button 
+                                    onClick={() => selectActivity(activity.id)} 
+                                    floated='right' 
+                                    content='View' 
+                                    color='blue' />
+                                <Button
+                                    name={activity.id}
+                                    loading={target === activity.id && submitting} onClick={(e) => deleteActivity(e, activity.id)} 
+                                    floated='right' 
+                                    content='Delete' 
+                                    color='red' />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
