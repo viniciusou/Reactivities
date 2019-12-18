b4a7316a114d49cb3d6b0a1e91b2a51e5ec6b81f
viniciusou <viniciusou@gmail.com>
2019-12-18 17:37:45 -0300

    Added routing to react app

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -7,10 +7,8 @@ configure({enforceActions: 'always'});
 
 class ActivityStore {
     @observable activityRegistry = new Map();
-    @observable activities: IActivity[] = [];
-    @observable selectedActivity: IActivity | undefined;
+    @observable activity: IActivity | null = null;
     @observable loadingInitial = false;
-    @observable editMode = false;
     @observable submitting = false;
     @observable target = '';
 
@@ -38,13 +36,41 @@ class ActivityStore {
         }
     };
 
+    @action loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if (activity) {
+            this.activity = activity;
+        } else {
+            this.loadingInitial = true;
+            try{
+                activity = await agent.Activities.details(id);
+                runInAction('getting activity', () => {
+                    this.activity = activity;
+                    this.loadingInitial = false;
+                }); 
+            } catch(error) {
+                runInAction('getting activity false', () => {
+                    this.loadingInitial = false;
+                });
+                console.log(error);
+            }
+        }
+    };
+
+    @action clearActivity = () => {
+        this.activity = null;
+    };
+
+    getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    };
+
     @action createActivity = async (activity: IActivity) => {
         this.submitting = true;
         try {
             await agent.Activities.create(activity);
             runInAction('creating activity', () => {
                 this.activityRegistry.set(activity.id, activity);
-                this.editMode = false;
                 this.submitting = false;
             });
         } catch(error) {
@@ -61,8 +87,7 @@ class ActivityStore {
             await agent.Activities.update(activity);
             runInAction('editing activity', () => {
                 this.activityRegistry.set(activity.id, activity);
-                this.selectedActivity = activity;
-                this.editMode = false;
+                this.activity = activity;
                 this.submitting = false;
             });
         } catch(error) {    
@@ -93,28 +118,6 @@ class ActivityStore {
         }
     };
 
-    @action openCreateForm = () => {
-        this.editMode = true;
-        this.selectedActivity = undefined;
-    };
-
-    @action openEditForm = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-        this.editMode = true;
-    };
-
-    @action cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    };
-
-    @action cancelFormOpen = () => {
-        this.editMode = false;
-    }
-
-    @action selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-        this.editMode = false;
-    };
 }
 
 export default createContext(new ActivityStore())
