0ca26c3b0b76ce4d6fa262ae5b7b539922a18268
viniciusou <viniciusou@gmail.com>
2019-12-31 19:43:36 -0300

    Added authorization to client side

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,7 +2,6 @@ import React, {useState, useContext, useEffect} from 'react'
 import { Segment, Form, Button, Grid } from 'semantic-ui-react'
 import { ActivityFormValues } from '../../../app/models/activity'
 import {v4 as uuid} from 'uuid';
-import ActivityStore from '../../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
 import { RouteComponentProps } from 'react-router-dom';
 import { Form as FinalForm, Field } from 'react-final-form';
@@ -13,6 +12,7 @@ import DateInput from '../../../app/common/form/DateInput';
 import { category } from '../../../app/common/options/categoryOptions';
 import { combineDateAndTime } from '../../../app/common/util/util';
 import { combineValidators, isRequired, composeValidators, hasLengthGreaterThan } from 'revalidate';
+import { RootStoreContext } from '../../../app/stores/rootStore';
 
 const validate = combineValidators({
     title: isRequired({message: 'The event title is required'}),
@@ -32,13 +32,13 @@ interface DetailParams {
 }
 
 const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, history}) => {
-    const activityStore = useContext(ActivityStore);
+    const rootStore = useContext(RootStoreContext);
     const {
         createActivity, 
         editActivity, 
         submitting, 
         loadActivity,
-    } = activityStore;
+    } = rootStore.activityStore;
 
     const [activity, setActivity] = useState(new ActivityFormValues());
     const [loading, setLoading] = useState(false);
--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import { Menu, Container, Button } from 'semantic-ui-react'
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from 'react'
+import { Menu, Container, Button, Dropdown, Image } from 'semantic-ui-react'
+import { NavLink, Link } from 'react-router-dom';
+import { RootStoreContext } from '../../app/stores/rootStore';
 
 export const NavBar: React.FC = () => {
+    const rootStore = useContext(RootStoreContext);
+    const { user, logout } = rootStore.userStore;
     return (
         <Menu fixed='top' inverted>
             <Container>
@@ -14,6 +17,17 @@ export const NavBar: React.FC = () => {
             <Menu.Item>
                 <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
             </Menu.Item>
+            {user &&
+                <Menu.Item position='right'>
+                    <Image avatar spaced='right' src={user.image || '/assets/user.png'} />
+                    <Dropdown pointing='top left' text={user.displayName}>
+                    <Dropdown.Menu>
+                        <Dropdown.Item as={Link} to={`/profile/username`} text='My profile' icon='user'/>
+                        <Dropdown.Item onClick={logout} text='Logout' icon='power' />
+                    </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item>
+            }
             </Container>
         </Menu>
     )
