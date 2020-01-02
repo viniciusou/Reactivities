a7affa6c2a48ec94c04ca9b48e838a0635f292e1
viniciusou <viniciusou@gmail.com>
2020-01-02 20:29:55 -0300

    Added photo upload to client side

--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -22,6 +22,7 @@ const activityImageTextStyle = {
 const ActivityDetailedHeader: React.FC<{activity: IActivity}> = ({activity}) => {
     const rootStore = useContext(RootStoreContext);
     const {attendActivity, cancelAttendance, loading} = rootStore.activityStore;
+    const host = activity.attendees.filter(x => x.isHost)[0];
     return (
         <Segment.Group>
             <Segment basic attached='top' style={{ padding: '0' }}>
@@ -37,7 +38,7 @@ const ActivityDetailedHeader: React.FC<{activity: IActivity}> = ({activity}) => 
                         />
                         <p>{format(activity.date, 'eeee do MMMM')}</p>
                         <p>
-                            Hosted by <strong>Bob</strong>
+                            Hosted by <Link to={`/profile/${host.username}`}><strong>{host.displayName}</strong></Link>
                         </p>
                         </Item.Content>
                     </Item>
